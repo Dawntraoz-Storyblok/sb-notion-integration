80161bc8d34fad6d4e8dffb20f7abf80e591b490
Dawntraoz <albasilventefu@gmail.com>
2023-12-04 15:15:15 +0100

    chore: initial commit

---
+++ src/components/FieldPluginExample/Counter.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: 'onIncrease'): void
+}>()
+const props = defineProps<{ count: number }>()
+
+const onClick = () => {
+  emit('onIncrease')
+}
+</script>
+
+<template>
+  <div>
+    <h2>Field Value</h2>
+    <div class="counter-value">
+      {{ props.count }}
+    </div>
+    <button
+      class="btn w-full"
+      @click="onClick"
+    >
+      Increment
+    </button>
+  </div>
+</template>
